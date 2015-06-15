c60d123e9bc5b2e2ee8c7934fe79c46ca2d761df
unknown <D:\MAIL>
2015-06-15 22:38:16 +0500

    0.2.10

--- lib/src/dart_utils.dart
+++ lib/src/dart_utils.dart
@@ -2,13 +2,11 @@ part of ccompile.ccompile;
 
 class DartUtils {
   static String getSdkFolder() {
-    var path = Platform.environment['DART_SDK'];
-    if (path == null) {
-      return null;
-    }
-
-    if (path.endsWith('\\') || path.endsWith('/')) {
-      path = path.substring(0, path.length - 1);
+    var path = DartSDK.path;
+    if (path != null) {
+      if (path.endsWith('\\') || path.endsWith('/')) {
+        path = path.substring(0, path.length - 1);
+      }
     }
 
     return path;
